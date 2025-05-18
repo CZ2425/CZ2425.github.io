be6423fe3ee4053e08719e1e16c3bf37ed5b7094
Chuanliang Zhu <charleszhu2015@hotmail.com>
2025-05-18 10:44:13 -0700

    Update C#.cs

--- C#.cs
+++ C#.cs
@@ -1,42 +1,37 @@
-const int redPin = 3;
-const int greenPin = 5;
-const int bluePin = 6;
+using UnityEngine;
 
-void setup() {
-  
-  Serial.begin(9600);
-  
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(3, OUTPUT);
+public class PlayerController : MonoBehaviour
+{
+public float speed = 5f;
+public float jumpForce = 10f;
+private bool isGrounded;
+private Rigidbody2D rb;
+
+void Start()
+{
+rb = GetComponent<Rigidbody2D>();
 }
 
-void loop() {
-  
-  while (Serial.available() > 0) {
+void Update()
+{
+// Horizontal Movement
+float moveInput = Input.GetAxis("Horizontal");
+rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-    
-    int red = Serial.parseInt();
-    int green = Serial.parseInt();
-    int blue = Serial.parseInt();
-
-    
-    if (Serial.read() == '\n') {
-      
-      red = 255 - constrain(red, 0, 255);
-      green = 255 - constrain(green, 0, 255);
-      blue = 255 - constrain(blue, 0, 255);
-
-
-      analogWrite(5, red);
-      analogWrite(4, green);
-      analogWrite(3, blue);
-
-      
-      Serial.print(red, HEX);
-      Serial.print(green, HEX);
-      Serial.println(blue, HEX);
-    }
-  }
+// Jumping
+if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+{
+rb.velocity = Vector2.up * jumpForce;
+isGrounded = false;
+}
 }
 
+void OnCollisionEnter2D(Collision2D collision)
+{
+if (collision.gameObject.CompareTag("Ground"))
+{
+isGrounded = true;
+}
+}
+}
+  
